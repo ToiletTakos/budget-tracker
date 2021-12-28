4e1fb0fa0f6ffa2d45c94829afba060c54e5ec9c
Dan <dan.pandolfo@gmail.com>
2021-12-28 17:33:11 -0500

    final touches to functionality files, added readme with screenshots

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,25 +3,25 @@ const DATA_CACHE_NAME = 'budget-tracker-data-cache-v1';
 
 const FILES_TO_CACHE = [
   '/',
-  '/index.html',
-  '/manifest.json',
-  '/js/index.js',
-  '/css/style.css',
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
-  '/icons/icon-512x512.png'
+  './index.html',
+  './manifest.json',
+  './js/index.js',
+  './css/styles.css',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png'
 ];
 
 // Install the service worker
 self.addEventListener('install', function(evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log('Your files were pre-cached successfully!');
+      console.log('installing cache : ' + CACHE_NAME);
       return cache.addAll(FILES_TO_CACHE);
     })
   );
